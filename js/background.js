4df157a54a470120a3a534d3170d54e0c883e7ee
SSRyu <jjkztok3@gmail.com>
2021-09-04 15:12:17 +0900

    Modify body height for gradient

--- js/background.js
+++ js/background.js
@@ -7,3 +7,9 @@ for (let i = 0; i < 7; ++i) {
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 document.body.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), 80%, rgba(0, 0, 0, 0.7), 90%, rgba(0, 0, 0, 1.0)), url(img/${chosenImage})`;
+
+function onResizeWindow() {
+  document.body.style.height = `${document.body.clientWidth * 0.7}px`;
+}
+
+window.onresize = onResizeWindow;
