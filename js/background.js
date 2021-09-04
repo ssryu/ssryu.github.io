97e463f295518dbc472df7cd987c95c6ae7b1f13
SSRyu <jjkztok3@gmail.com>
2021-09-04 14:49:05 +0900

    submit v1

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = [];
+
+for (let i = 0; i < 7; ++i) {
+  images.push(`${i}.jpg`.padStart(6, "0"));
+}
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+document.body.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), 80%, rgba(0, 0, 0, 0.7), 90%, rgba(0, 0, 0, 1.0)), url(img/${chosenImage})`;
